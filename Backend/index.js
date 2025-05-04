62960c7f899f8d0cd928208c656bd0e618804752
Mahek Bhartiya <youremail@example.com>
2025-05-05 01:05:49 +0530

    Final commit message for merge

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,7 @@ const app = express();
 
 // Database Connection
 connectDB(process.env.DB_URL);
+console.log(`Database connected successfully at ${process.env.DB_URL}`);
 
 // Middleware
 app.use(cors({ origin: 'http://localhost:3000', credentials: true })); // Enable CORS for localhost:3000
@@ -26,6 +27,6 @@ app.use(cookieParser()); // Use cookie-parser
 app.use(Router); // Use the router for handling routes
 
 // Start the server
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 8000, ()=>{
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
