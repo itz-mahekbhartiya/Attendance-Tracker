db1d23a342605e350e67af06eb3a39f76a3d1987
Mahek Bhartiya <youremail@example.com>
2025-05-05 21:12:18 +0530

    cookie

--- Backend/Controller/coreController.js
+++ Backend/Controller/coreController.js
@@ -5,23 +5,19 @@ const {Admin} = require('../Model/Admin');
 
 async function loginEmployee(req, res) {
     const { EmpId, Password } = req.body;
-    console.log(req.body);
     try {
         const user = await Employee.findOne({ EmpId });
         if (!user) return res.status(400).json({ message: 'User not found' });
 
-        // const isPasswordValid = await bcrypt.compare(Password, user.Password);
         if (user.Password !== Password) return res.status(400).json({ message: 'Invalid password' });
 
-        // Create JWT token with user ID
-        // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        // Set token in cookies
-        const cokkies = res.cookie('EmpId', EmpId, {
-            httpOnly: false,
-            secure: false, // Set to true only in production
-            sameSite: 'lax', // Allow cookies on cross-origin
-            maxAge: 60 * 60 * 1000 // 1 hour
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: 60 * 60 * 1000
         });
 
         return res.status(200).json({ success: true, user_id: user._id, message: 'Logged in successfully' });
